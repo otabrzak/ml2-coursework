fa7232bfee71e1f6bdf516c23593da9a5ec01ca5
Lucie Pluskalova <pluskalova.luc@gmail.com>
2025-10-27 10:07:55 +0100

    make eda notebook and started

--- EDA_first_xploration.py
+++ EDA_first_xploration.py
@@ -1,1 +1,9 @@
 # just made the file, but then run into trouble
+
+import pandas as pd
+
+df0 = pd.read_csv(r"ready_data/data_a0_encoded.csv")
+df1 = pd.read_csv(r"ready_data/data_a1_encoded.csv")
+
+print(df0.describe().T)
+
